d9cacafeeebba08e97d9944595a91cf0d6e78d56
Matthew Healy <matthew@liamhealy.xyz>
2022-12-15 22:44:00 +0100

    Replace rust tests with uplp files

--- tests/integration/main.rs
+++ tests/integration/main.rs
@@ -1,6 +1,57 @@
-mod binary_ops;
-mod common;
-mod functions;
-mod let_binding;
-mod number_literals;
-mod unit;
+use serde::{Deserialize, Serialize};
+use std::{fs::File, io::Read, path::PathBuf};
+use test_generator::test_resources;
+use untitled_programming_language_project::{evaluate, values::Val};
+
+#[test_resources("./examples/pass/**/*.uplp")]
+pub fn test_example_file(p: &str) {
+    let path = {
+        let proj_root = env!("CARGO_MANIFEST_DIR");
+        PathBuf::from(proj_root).join(p)
+    };
+
+    let file_contents = {
+        let mut file = File::open(path).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        contents
+    };
+
+    let (annotation, program) = file_contents
+        .split_once('\n')
+        .expect("Could not extract expectation from test file.");
+
+    let a = annotation
+        .strip_prefix("-- test: ")
+        .expect("Malformed test annotation");
+
+    match a {
+        "skip" => (),
+        s if s.starts_with("check ") => {
+            let json = s.strip_prefix("check ").unwrap();
+            let e: TestExpectation =
+                serde_json::from_str(json).expect("Malformed test annotation json");
+
+            let result = evaluate(program).expect("Program evaluation failed");
+
+            assert_eq!(Val::from(e), result);
+        }
+        _ => panic!("Unsupported test annotation."),
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(tag = "type", content = "value")]
+enum TestExpectation {
+    Num(f64),
+    Unit {},
+}
+
+impl From<TestExpectation> for Val {
+    fn from(e: TestExpectation) -> Self {
+        match e {
+            TestExpectation::Num(n) => Val::Num(n),
+            TestExpectation::Unit {} => Val::Unit,
+        }
+    }
+}
